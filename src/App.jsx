9a449c4afddde6ff2cfb0b3050330b1a20561453
amirreza79ahmadi <amirreza79ahmadi@gmail.com>
2023-07-29 16:12:51 +0330

    start

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,7 @@ import Layouts from './layouts/layouts'
 export default function App() {
   return (
     <Layouts>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ea odit corporis corrupti consequatur temporibus odio nostrum id eligendi. Iure iste voluptatum ipsa ab optio dicta fugiat fugit exercitationem in.
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ea odit corporis corrupti consequatur temporibus odio nostrum id eligendi. Iure iste voluptatum ipsa ab optio dicta fugiat fugit exercitationem in.
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ea odit corporis corrupti consequatur temporibus odio nostrum id eligendi. Iure iste voluptatum ipsa ab optio dicta fugiat fugit exercitationem in.
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ea odit corporis corrupti consequatur temporibus odio nostrum id eligendi. Iure iste voluptatum ipsa ab optio dicta fugiat fugit exercitationem in.
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ea odit corporis corrupti consequatur temporibus odio nostrum id eligendi. Iure iste voluptatum ipsa ab optio dicta fugiat fugit exercitationem in.
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ea odit corporis corrupti consequatur temporibus odio nostrum id eligendi. Iure iste voluptatum ipsa ab optio dicta fugiat fugit exercitationem in.
+      
     </Layouts>
   )
 }
