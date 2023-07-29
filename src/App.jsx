127fe8b87a96c8032b780596ff3f48ec61873663
amirreza79ahmadi <amirreza79ahmadi@gmail.com>
2023-07-29 16:08:15 +0330

    create component layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Layouts from './layouts/layouts'
 
 export default function App() {
   return (
-    <div className="super-app-container">
-      
-    </div>
+    <Layouts>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ea odit corporis corrupti consequatur temporibus odio nostrum id eligendi. Iure iste voluptatum ipsa ab optio dicta fugiat fugit exercitationem in.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ea odit corporis corrupti consequatur temporibus odio nostrum id eligendi. Iure iste voluptatum ipsa ab optio dicta fugiat fugit exercitationem in.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ea odit corporis corrupti consequatur temporibus odio nostrum id eligendi. Iure iste voluptatum ipsa ab optio dicta fugiat fugit exercitationem in.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ea odit corporis corrupti consequatur temporibus odio nostrum id eligendi. Iure iste voluptatum ipsa ab optio dicta fugiat fugit exercitationem in.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ea odit corporis corrupti consequatur temporibus odio nostrum id eligendi. Iure iste voluptatum ipsa ab optio dicta fugiat fugit exercitationem in.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore ea odit corporis corrupti consequatur temporibus odio nostrum id eligendi. Iure iste voluptatum ipsa ab optio dicta fugiat fugit exercitationem in.
+    </Layouts>
   )
 }
---
+++ src/layouts/layouts.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Layouts(props) {
+  return (
+    <div className="super-app-container">
+        {props.children}
+    </div>
+  )
+}
+
