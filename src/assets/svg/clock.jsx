a488ec2518aa26bcd8f040fe50269f4750fb0217
amirreza79ahmadi <amirreza79ahmadi@gmail.com>
2023-07-30 13:03:22 +0330

    end compoennt home

---
+++ src/assets/svg/clock.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function Clock() {
+  return (
+    <svg
+      width="9"
+      height="9"
+      viewBox="0 0 9 9"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g opacity="0.7" clipPath="url(#clip0_26_195)">
+        <path
+          d="M4.46721 7.73141C6.25181 7.73141 7.69851 6.2847 7.69851 4.5001C7.69851 2.7155 6.25181 1.2688 4.46721 1.2688C2.6826 1.2688 1.2359 2.7155 1.2359 4.5001C1.2359 6.2847 2.6826 7.73141 4.46721 7.73141Z"
+          fill="#9FA1AD"
+          stroke="#9FA1AD"
+          strokeWidth="1.43614"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M4.46722 2.70483V4.5L5.54433 5.5771"
+          stroke="#F9F9F9"
+          strokeWidth="0.718068"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_26_195">
+          <rect
+            width="8.61681"
+            height="8.61681"
+            fill="white"
+            transform="translate(0.158813 0.19165)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,11 +1,23 @@
 import React from "react";
-import { Box } from "@mui/material";
-import styles from "../../assets/sass/home.module.scss";
 import { NavLink } from "react-router-dom";
+import { Splide, SplideTrack, SplideSlide } from "@splidejs/react-splide";
+import {
+  Box,
+  Grid,
+  Typography,
+  Button,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Card,
+} from "@mui/material";
 import Avatar from "@mui/material/Avatar";
+import styles from "../../assets/sass/home.module.scss";
 import ArrowSmallLeftIcon from "../../assets/svg/arrow-small-left";
-import Grid from "@mui/material/Grid";
 import GraduationCapIcon from "../../assets/svg/graduation-cap";
+import FireIcon from "../../assets/svg/fire";
+import ClockIcon from "../../assets/svg/clock";
+import StartIcon from "../../assets/svg/star";
 
 export default function Home() {
   return (
@@ -80,11 +92,76 @@ export default function Home() {
       </div>
 
       <div className={styles.box_title_study}>
-        <GraduationCapIcon/>
-        <h1>مرکـــز  آموزش رسادنت  🥳</h1>
+        <GraduationCapIcon />
+        <h1>مرکـــز آموزش رسادنت 🥳</h1>
       </div>
+      <Splide
+        hasTrack={false}
+        options={{
+          direction: "rtl",
+          pagination: false,
+          arrows: false,
+          padding: { left: 40, right: 0 },
+        }}
+      >
+        <SplideTrack>
+          <SplideSlide>
+            <NavLink className={styles.text_dec_none}>
+              <Card
+                sx={{
+                  maxWidth: "314px",
+                  p: 1,
+                  borderRadius: "13px",
+                  position: "relative",
+                }}
+              >
+                <div className={styles.rating}>
+                  <span className={styles.text_rating}>4.0</span>
+                  <StartIcon />
+                </div>
+                <CardMedia
+                  className={styles.img_fluid}
+                  component="img"
+                  alt="green iguana"
+                  height="140"
+                  image="/image/Image-Heart.png"
+                />
+                <CardContent sx={{ p: 1 }}>
+                  <Typography className={styles.title_card} variant="body2">
+                    آموزش بخش ثبت سفارش در رساجت
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Typography
+                    sx={{ ml: 1 }}
+                    className={styles.details_card}
+                    variant="span"
+                  >
+                    <ClockIcon />
+                    1h 25min
+                  </Typography>
+                  <Typography className={styles.details_card} variant="span">
+                    <FireIcon />
+                    آموزش و توسعه
+                  </Typography>
+                </CardActions>
+              </Card>
+            </NavLink>
+          </SplideSlide>
+        </SplideTrack>
+      </Splide>
 
-
+      <Grid sx={{ mt: 5 }} container spacing={3}>
+        <Grid  xs={4}>
+          <NavLink className={styles.btn_secondary}><span>قوانین و مقررات</span></NavLink>
+        </Grid>
+        <Grid  xs={4}>
+          <NavLink className={styles.btn_primary}><span>درباره رسادنت</span></NavLink>
+        </Grid>
+        <Grid  xs={4}>
+          <NavLink className={styles.btn_secondary}><span>حریم خصوصی</span></NavLink>
+        </Grid>
+      </Grid>
     </Box>
   );
 }
