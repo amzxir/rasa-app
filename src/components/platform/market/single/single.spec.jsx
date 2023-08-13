7804e3f55d15b4814783372e7793ba9d279e3008
amirreza79ahmadi <amirreza79ahmadi@gmail.com>
2023-08-13 17:51:47 +0330

    asd

--- src/components/platform/market/single/single.spec.jsx
+++ src/components/platform/market/single/single.spec.jsx
@@ -1,14 +1,21 @@
-import { render, fireEvent, screen } from "@testing-library/react";
+import { shallow } from 'enzyme';
 import Single from "./single";
-import '@testing-library/jest-dom'
 
-
-test("should first", () => {
-  render(<Single />);
-
-  const counter = screen.getByTestId("counter");
-  const incrementBtn = screen.getByTestId("handelTotal");
-
-  fireEvent.click(incrementBtn);
-  expect(counter).toHaveTextContent("1");
+// Testing
+describe('MyComponent', () => {
+  it('should re-render component when state is updated', () => {
+    const wrapper = shallow(<Single />);
+    expect(wrapper.find('p').text()).toBe('0');
+    wrapper.setState({ count: 1 });
+    expect(wrapper.find('p').text()).toBe('1');
+  });
 });
+// test("should first", () => {
+//   render(<Single />);
+
+//   const counter = screen.getByTestId("counter");
+//   const incrementBtn = screen.getByTestId("handelTotal");
+
+//   fireEvent.click(incrementBtn);
+//   expect(counter).toHaveTextContent("1");
+// });
