d8ffbae99a0d94dc6e71d18b5c007c29e14ad0a9
amirreza79ahmadi <amirreza79ahmadi@gmail.com>
2023-09-13 12:05:09 +0330

    change logo in splash screen and scroll top in ref

--- src/layouts/layouts.jsx
+++ src/layouts/layouts.jsx
@@ -1,11 +1,11 @@
-import React, { useContext } from "react";
-import Header from "./header";
+import React, { useContext , useEffect , useRef } from "react";
 import { useTheme } from "@mui/material/styles";
 import { Box, Container } from "@mui/material";
+import { useLocation } from "react-router-dom";
+import Header from "./header";
 import Menu from "./menu";
 import ColorModeContext from "../context/color-mode-context";
 import Splash from "../components/splash/splash";
-import { useLocation } from "react-router-dom";
 
 export default function Layouts(props) {
   // start function darkmode
@@ -16,24 +16,18 @@ export default function Layouts(props) {
   let location = useLocation();
   let path = location.pathname;
 
+  // start function scroll top
+  const ref = useRef()
+  useEffect(() => ref.current.scrollTo(0, 0));
+  // start function scroll top 
+
   return (
     <Box
-      className={
-        theme.palette.mode === "light"
-          ? "super-app-container-light"
-          : "super-app-container-dark"
-      }
-    >
+      className={theme.palette.mode === "light" ? "super-app-container-light" : "super-app-container-dark"}>
       {path !== "/login" ? <Header /> : null}
       <Container maxWidth="sm">
         <Splash />
-        <div
-          className={
-            theme.palette.mode === "light"
-              ? "scroll-auto-light"
-              : "scroll-auto-dark"
-          }
-        >
+        <div ref={ref} className={theme.palette.mode === "light" ? "scroll-auto-light" : "scroll-auto-dark"}>
           {props.children}
         </div>
       </Container>
