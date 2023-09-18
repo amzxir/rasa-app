776b2d379fdab11cd7a845ed0148aca9bb6e7a59
amirreza79ahmadi <amirreza79ahmadi@gmail.com>
2023-09-18 11:19:49 +0330

    send message in rasa jet compelete

--- src/components/platform/jet/jet-text.jsx
+++ src/components/platform/jet/jet-text.jsx
@@ -1,7 +1,7 @@
-import React, { useContext, useState , useEffect } from "react";
+import React, { useContext, useState , useRef, useEffect } from "react";
 import { Box, IconButton, Menu, MenuItem } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
-import { useForm } from "react-hook-form";
+import { set, useForm } from "react-hook-form";
 import LightStyles from "../../../assets/sass/light/jet/text.module.scss";
 import DarkStyles from "../../../assets/sass/dark/jet/text.module.scss";
 import ColorModeContext from "../../../context/color-mode-context";
@@ -26,25 +26,43 @@ export default function JetText() {
   };
   // end function and state dropdown
 
-  // start react hook form and send chat
+  // start send chat
+  const [input, setInput] = useState();
 
+  const [message , setMessage] = useState([]);
 
+  const handelChange = (e) => {
+    setInput(e.target.value)
+  }
 
-  // end react hook form and send chat
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    const id = message.length + 1;
+    setMessage((prev) => [
+      ...prev,
+      {
+        id: id,
+        message: input,
+        complete: false,
+      },
+    ]);
+    setInput("");
+  }
+  // end send chat
+
+  // start function and state scroll bottom in message
+  const bottomRef = useRef(null);
+
+  useEffect(() => {
+    // 👇️ scroll to bottom every time messages change
+    bottomRef.current?.scrollIntoView({behavior: 'smooth'});
+  }, [message]);
+  // end function and state scroll bottom in message
 
 
   return (
     <Box sx={{ mt: 5, mb: 5 }} className={theme.palette.mode === "light" ? LightStyles.jet_text : DarkStyles.jet_text}>
 
-        <div className={theme.palette.mode === "light" ? LightStyles.chat_right : DarkStyles.chat_right}>
-           <p>
-           سلام ، پویا رستمی هستم
-           <br />
-
-           </p>
-          <span className={theme.palette.mode === "light" ? LightStyles.date : DarkStyles.date}>1402/2/4</span>
-         </div>
- 
       <div className={theme.palette.mode === "light" ? LightStyles.chat_left : DarkStyles.chat_left}>
         <p>
         سلام ، پویا رستمی هستم
@@ -54,8 +72,17 @@ export default function JetText() {
        <span className={theme.palette.mode === "light" ? LightStyles.date : DarkStyles.date}>1402/2/4</span>
       </div>
 
+      {message.map((m) => {
+          return (
+          <div className={theme.palette.mode === "light" ? LightStyles.chat_right : DarkStyles.chat_right}>
+            <p>{m.message}</p>
+            <span className={theme.palette.mode === "light" ? LightStyles.date : DarkStyles.date}>1402/2/4</span>
+          </div>
+          );
+      })}
+
       <div className={theme.palette.mode === "light" ? LightStyles.send : DarkStyles.send}>
-        <form  className={theme.palette.mode === "light" ? LightStyles.form_group : DarkStyles.form_group}>
+        <form onSubmit={handelSubmit} className={theme.palette.mode === "light" ? LightStyles.form_group : DarkStyles.form_group}>
           <IconButton onClick={handleClick} className={theme.palette.mode === "light" ? LightStyles.btn_item : DarkStyles.btn_item}>
             <PlusIcon />
           </IconButton>
@@ -80,12 +107,13 @@ export default function JetText() {
               </p>
             </MenuItem>
           </Menu>
-          <input type="text" placeholder={fa["Note your text"]} />
-          <IconButton className={theme.palette.mode === "light" ? LightStyles.btn_send : DarkStyles.btn_send}>
+          <input type="text" placeholder={fa["Note your text"]} value={input} onChange={handelChange} />
+          <IconButton onClick={handelSubmit} className={theme.palette.mode === "light" ? LightStyles.btn_send : DarkStyles.btn_send}>
             <SendIcon />
           </IconButton>
         </form>
       </div>
+      <div ref={bottomRef} />
     </Box>
   );
 }
