4cac27e60679d8b822357e22ddbfac7b0133937f
amirreza79ahmadi <amirreza79ahmadi@gmail.com>
2023-09-20 17:16:55 +0430

    media query in menu

--- src/assets/svg/Category.jsx
+++ src/assets/svg/Category.jsx
@@ -10,40 +10,40 @@ export default function Category() {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M20.7504 5.22995C20.7504 7.4276 18.9681 9.2099 16.7704 9.2099C14.5728 9.2099 12.7915 7.4276 12.7915 5.22995C12.7915 3.0323 14.5728 1.25 16.7704 1.25C18.9681 1.25 20.7504 3.0323 20.7504 5.22995Z"
         stroke="#374957"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M9.20893 5.22995C9.20893 7.4276 7.4276 9.2099 5.22898 9.2099C3.0323 9.2099 1.25 7.4276 1.25 5.22995C1.25 3.0323 3.0323 1.25 5.22898 1.25C7.4276 1.25 9.20893 3.0323 9.20893 5.22995Z"
         stroke="#374957"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M20.7504 16.7004C20.7504 18.8981 18.9681 20.6794 16.7704 20.6794C14.5728 20.6794 12.7915 18.8981 12.7915 16.7004C12.7915 14.5028 14.5728 12.7205 16.7704 12.7205C18.9681 12.7205 20.7504 14.5028 20.7504 16.7004Z"
         stroke="#374957"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M9.20893 16.7004C9.20893 18.8981 7.4276 20.6794 5.22898 20.6794C3.0323 20.6794 1.25 18.8981 1.25 16.7004C1.25 14.5028 3.0323 12.7205 5.22898 12.7205C7.4276 12.7205 9.20893 14.5028 9.20893 16.7004Z"
         stroke="#374957"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
