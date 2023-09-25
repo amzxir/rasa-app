f45dfbcf1b22dde51adcc00e0789869d18c76b6a
amirreza79ahmadi <amirreza79ahmadi@gmail.com>
2023-09-25 15:12:44 +0330

    asdasd

--- src/layouts/menu.jsx
+++ src/layouts/menu.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { NavLink, useLocation } from "react-router-dom";
-import { Container } from "@mui/material";
+import { Container, IconButton } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import LightStyles from "../assets/sass/light/menu.module.scss";
 import DarkStyles from "../assets/sass/dark/menu.module.scss";
@@ -23,72 +23,35 @@ export default function Menu() {
   let path = location.pathname;
   // end search location
   return path === "/jet/send" ? null : (
-    <div
-      className={
-        theme.palette.mode === "light"
-          ? LightStyles.menu_bar
-          : DarkStyles.menu_bar
-      }
-    >
-      <Container
-        className={
-          theme.palette.mode === "light" ? LightStyles.flex : DarkStyles.flex
-        }
-        sx={{ p: 2 }}
-        maxWidth="sm"
-      >
-        <NavLink
-          to={"/"}
-          className={
-            theme.palette.mode === "light" ? LightStyles.pr_1 : DarkStyles.pr_1
-          }
-        >
-          <HomeIcon />
+    <div className={theme.palette.mode === "light" ? LightStyles.menu_bar : DarkStyles.menu_bar}>
+      <Container className={theme.palette.mode === "light" ? LightStyles.flex : DarkStyles.flex} sx={{ p: 1 }} maxWidth="sm">
+        <NavLink to={"/"} className={theme.palette.mode === "light" ? LightStyles.pr_1 : DarkStyles.pr_1}>
+          <IconButton>
+            <HomeIcon />
+          </IconButton>
         </NavLink>
-        <NavLink
-          to={"/shop/card"}
-          state={fa["card"]}
-          className={
-            theme.palette.mode === "light" ? LightStyles.pl_3 : DarkStyles.pl_3
-          }
-        >
+        <NavLink to={"/shop/card"} state={fa["card"]} className={theme.palette.mode === "light" ? LightStyles.pl_3 : DarkStyles.pl_3}>
+        <IconButton>
           <ShoppingCartIcon />
+        </IconButton>
         </NavLink>
-        <NavLink
-          to={"/shop"}
-          className={
-            theme.palette.mode === "light"
-              ? LightStyles.shop_box
-              : DarkStyles.shop_box
-          }
-        >
+        <NavLink to={"/shop"} className={theme.palette.mode === "light" ? LightStyles.shop_box : DarkStyles.shop_box}>
           <span>
             <ShopIcon />
-            <span
-              className={
-                theme.palette.mode === "light"
-                  ? LightStyles.bage
-                  : DarkStyles.bage
-              }
-            >
+            <span className={theme.palette.mode === "light" ? LightStyles.bage : DarkStyles.bage}>
               4
             </span>
           </span>
         </NavLink>
-        <NavLink
-          className={
-            theme.palette.mode === "light" ? LightStyles.pr_3 : DarkStyles.pr_3
-          }
-        >
+        <NavLink className={theme.palette.mode === "light" ? LightStyles.pr_3 : DarkStyles.pr_3}>
+        <IconButton>
           <CommentsIcon />
+        </IconButton>
         </NavLink>
-        <NavLink
-          to={"/profile"}
-          className={
-            theme.palette.mode === "light" ? LightStyles.pl_1 : DarkStyles.pl_1
-          }
-        >
+        <NavLink to={"/profile"} className={theme.palette.mode === "light" ? LightStyles.pl_1 : DarkStyles.pl_1}>
+        <IconButton>
           <UserIcon />
+        </IconButton>
         </NavLink>
       </Container>
     </div>
