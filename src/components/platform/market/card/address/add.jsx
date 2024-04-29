d9c26e86ee9395649e1d97884b6dd4c1afe7a7e9
amzxir <amirrreza79ahmadi@gmail.com>
2024-04-29 16:35:14 +0330

    refactor code add address

--- src/components/platform/market/card/address/add.jsx
+++ src/components/platform/market/card/address/add.jsx
@@ -23,12 +23,12 @@ export default function Add() {
 
   // start fetch state address
 
-  const [add , setAdd] = useState([]);
+  const [add, setAdd] = useState([]);
 
   useEffect(() => {
     const address = localStorage.getItem("address");
     setAdd(JSON.parse(address))
-  },[])
+  }, [])
 
 
   // end fetch state address
@@ -36,24 +36,29 @@ export default function Add() {
   return (
     <FadeTransform in transformProps={{ exitTransform: 'translateX(-100px)' }} >
       <Box sx={{ mt: 5, mb: 5 }}>
-        {add?.map((i, index) => {
-          return (
-            i.address !== null && i.postalcode !== null && i.province !== null && i.receiver_mobile !== null && i.receiver_name !== null ?
-              <Card onClick={() => handelAddress(i)} key={i.id} sx={{ boxShadow: 0, borderRadius: '15px', p: 2, mb: 4 }}>
-                <div className={theme.palette.mode === "light" ? LightStyles.content_address : DarkStyles.content_address}>
-                  <PinIcon />
-                  <label htmlFor={`add_address${i.id}`} className={theme.palette.mode === "light" ? LightStyles.content : DarkStyles.content}>
-                    <h1 style={{ marginLeft: '5rem' }}>{i.receiver_name}</h1>
-                    <p>{i.adress.slice(1, 45)} ...</p>
-                  </label>
-                  <input name="add_address" id={`add_address${i.id}`} type="radio" />
-                </div>
-              </Card>
-
-              :
-              <></>
-          )
-        })}
+        {
+          add === null ?
+            <div style={{ display: 'grid', justifyContent: 'center', marginTop: '3rem', marginBottom:'3rem' }}>
+              <img style={{ maxWidth: '100%', height: 'auto', marginBottom: '.5rem' }} src="/image/address-svg.svg" alt="" />
+              <span style={{ color: theme.palette.mode === "light" ? '#000' : '#fff', textAlign: 'center' }}>آدرس یافت نشد برای ثبت ادرس روی دکمه زیر بروید</span>
+            </div>
+            :
+            add?.map((i) => {
+              return (
+                i.address !== null && i.postalcode !== null && i.province !== null && i.receiver_mobile !== null && i.receiver_name !== null &&
+                <Card onClick={() => handelAddress(i)} key={i.id} sx={{ boxShadow: 0, borderRadius: '15px', p: 2, mb: 4 }}>
+                  <div className={theme.palette.mode === "light" ? LightStyles.content_address : DarkStyles.content_address}>
+                    <PinIcon />
+                    <label htmlFor={`add_address${i.id}`} className={theme.palette.mode === "light" ? LightStyles.content : DarkStyles.content}>
+                      <h1 style={{ marginLeft: '5rem' }}>{i.receiver_name}</h1>
+                      <p>{i.adress.slice(1, 45)} ...</p>
+                    </label>
+                    <input name="add_address" id={`add_address${i.id}`} type="radio" />
+                  </div>
+                </Card>
+              )
+            })
+        }
         <NavLink to={"/shop/new-address"} state={fa["new address"]} className={theme.palette.mode === "light" ? LightStyles.btn_address : DarkStyles.btn_address}><span>{fa["add new address"]}</span></NavLink>
       </Box>
     </FadeTransform>
